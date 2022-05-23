7bc3f668142f482c767734bc5d943e59e7ff3ad8
dominique <domin@DESKTOP-OT2687Q>
2022-05-22 21:57:30 -0500

    next question funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,8 @@
-function start() {
-  timeRemaining = 75;
-  document.getElementById("timeRemaining").innerHTML = timeRemaining;
-
-  timer = setInterval(function () {
-    timeRemaining--;
-    document.getElementById("timeRemaining").innerHTML = timeRemaining;
-    if (timeRemaining <= 0) {
-      clearInterval(timer);
-      endGame();
-    }
-  }, 1000);
-
-  
-}
-
+var score = 0;
+var questionCurrent = -1;
+var timeRemaining = 0;
+var timer;
+// questions object array
 var questions = [
   {
     question: "Who lives in a pineapple under the sea?",
@@ -31,3 +20,46 @@ var questions = [
   answer: "d. Dogs"
 },
 ];
+
+function start() {
+  timeRemaining = 75;
+  document.getElementById("timeRemaining").innerHTML = timeRemaining;
+
+  timer = setInterval(function () {
+    timeRemaining--;
+    document.getElementById("timeRemaining").innerHTML = timeRemaining;
+    if (timeRemaining <= 0) {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+
+  nextquestion();
+}
+
+// call for next question
+function nextquestion() {
+  questionCurrent++;
+
+  if (questionCurrent > questions.length - 1) {
+    endGame();
+    return;
+  }
+
+  var quizContent = "<h2>" + questions[questionCurrent].question + "</h2>";
+
+  for (var x = 0; x < questions[questionCurrent].choices.length; x++) {
+   
+    var answerFunction =
+      questions[questionCurrent].choices[x] == questions[questionCurrent].answer
+        ? "correct()"
+        : "incorrect()";
+    var html = `<button onclick="${answerFunction}"> ${questions[questionCurrent].choices[x]}</button>`;
+    quizContent += html;
+  }
+  document.getElementById("quizContainer").innerHTML = quizContent;
+}
+
+function endGame() {
+  
+}
